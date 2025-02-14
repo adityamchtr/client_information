a64bcdebfc3543bd1f89a82a76db2d3cb069719f
Aditya Muchtar <106960735+adityamchtr@users.noreply.github.com>
2025-02-14 16:17:50 +0700

    remove registerWith

--- android/src/main/kotlin/kent/chien/client_information/ClientInformationPlugin.kt
+++ android/src/main/kotlin/kent/chien/client_information/ClientInformationPlugin.kt
@@ -13,7 +13,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** ClientInformationPlugin */
 class ClientInformationPlugin: FlutterPlugin, MethodCallHandler {
@@ -24,14 +23,6 @@ class ClientInformationPlugin: FlutterPlugin, MethodCallHandler {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "client_information")
     channel.setMethodCallHandler(this)
     this.context = flutterPluginBinding.applicationContext
-  }
-
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "client_information")
-      channel.setMethodCallHandler(ClientInformationPlugin())
-    }
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
